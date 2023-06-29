3734a3d3d99cd157aa232f08ad3a2b3f7299935a
Charles Onyango <charles.onyango@student.moringaschool.com>
2023-06-30 00:06:33 +0300

    complete

---
+++ db/migrate/20230629210042_passengers.rb
@@ -0,0 +1,10 @@
+class Passengers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :rides do |t|
+
+      t.references :taxi, foreign_key: true
+      t.references :passenger, foreign_key: true
+
+      t.timestamps
+  end
+end
